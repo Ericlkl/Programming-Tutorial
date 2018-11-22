fbb048ef556c89a42d241186feca5556fe72a6d8
Eric Lee <Raidenlkl@gmail.com>
2018-11-22 10:39:59 +1000

    JavaScript Variable, If-else condition ex added

---
+++ JavaScript/basic.js
@@ -0,0 +1,21 @@
+// Varaible Create
+var x = 1;  // int 數字類型
+var y = "Hello World"; // String 儲文字類型的
+var money = 100.50; // Double 儲有小數的數字
+var iamAustralian = false; // Boolean 儲 錯與對
+
+// Console.log
+console.log("Welcome to the first Class !!!");  // 用於打印在電腦視𥦬的指令
+
+// if - else if - else statement
+if ( x > 0){ // 首要的條件
+    console.log("x greater than zero!");
+}
+else if ( x == 0) // 次要的條件
+{
+    console.log("x is equal to zero !");
+}
+else //別的條件外情況
+{
+    console.log("x is lower than zero !");
+}
---
+++ JavaScript/basic_ex.js
@@ -0,0 +1,67 @@
+/*
+    創做一個Variable，叫做money, 儲300塊進去
+    創做一個Variable，叫做GU_student 為 true
+    創做一個Variable，叫做join_course 為 false
+*/
+
+/*
+    打印 "我現在開始要做Quiz了" <- 去電腦視𥦬
+    打印 "Section 1 : " <- 去電腦視𥦬
+    打印 "我現在有$ 'money的值'" <- 去電腦視𥦬
+*/
+
+/*
+    創做一個Variable，叫做 hours_per_day 為 24       <-這是儲存 一天有多少個小時
+    創做一個Variable，叫做 days_per_week 為 7        <-這是儲存 一星期有多少天
+    創做一個Variable，叫做 weeks_per_month 為 4.1    <-這是儲存 一個月有多少星期
+
+    創做一個Variable，叫做 hours_per_month           <- 問題 ： 這是儲存跟計算 一個月有多少個小時
+    計算方法 為 hours_per_day 乘 days_per_week 乘 weeks_per_month 
+    打印 "一個月有總共 'hours_per_month的值' 小時！ " <- 去電腦視𥦬
+*/
+
+/*
+    打印 "Section 2 : " <- 去電腦視𥦬
+    這個部分我們做一個 真實情況 條件類 的程序
+
+    創做一個 If/else 公式
+    如果你是 GU_student 的話， join_course 變為true
+    ------------------ 之後呢 ----------------------
+    打印 "能不能參加這個Course" <- 去電腦視𥦬
+    打印 join_course 的值 <- 去電腦視𥦬
+*/
+
+/*
+    創做一個 If/else if / else 公式
+    條件 1
+    如果 你的錢多過或者等於300的話
+    打印 "我現要去旅行了" <- 去電腦視𥦬
+*/
+
+/*
+    條件 2
+    如果 你的錢多過或者等於100的話
+    打印 "我現要去唱K了" <- 去電腦視𥦬
+*/
+
+
+/*
+    條件 3
+    如果 你的錢大於或者等於50的話
+    打印 "我現要去唱K了" <- 去電腦視𥦬
+*/
+
+/* 
+    其他條件/別的情況
+    打印 "我還是省點錢吧..." <- 去電腦視𥦬
+*/
+
+/*
+    打印 "Section 3 : " <- 去電腦視𥦬
+    這個部分我們儲做一個 澳元兌Rmb的程序
+
+    創做一個Variable，叫做 items_in_aud 為 14.9             <-這是儲存 物品在澳大利亞的價錢
+    創做一個Variable，叫做 aud_to_rmb 為 5.3041             <-這是儲存 澳元兌RMB的兌率
+    創做一個Variable，叫做 items_in_rmb 去計算 物品的價錢      <-這是儲存 物品在大陸的價錢
+    公式應該是 (澳洲物品價格 * 0.9) * 兌率
+*/
