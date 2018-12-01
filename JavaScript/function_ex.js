86caf267bb228e26d12a569d9f946f41186dd602
Eric Lee <Raidenlkl@gmail.com>
2018-12-01 12:13:02 +1000

    function ex added

---
+++ JavaScript/function_ex.js
@@ -0,0 +1,36 @@
+// Create a function called hello that print your name on the console
+
+// Create a function called Add has two input number1 and number2 
+// Return -> the add total from this two number
+
+// Create a function called sub has two input n1 and n2 
+// Return -> the sub total from this two number
+
+// Create a function called times has two input x and y 
+// Return -> the times total from this two number
+
+// Create a function called divid has two input a and b 
+// Return -> the divid total from this two number
+
+// Create a function called RunThisProgram 
+
+// At the beginning, run the hello function
+
+// 1. that execute a the add function to add two number
+// number 1 = 1 , number 2 = 2
+// console.log("The result is " + addTotal);
+
+// 2. that execute a the sub function to sub two number
+// n1 = 4 , n2 = 4
+// console.log("The result is " + subTotal);
+
+// 3. that execute a the times function to times two number
+// x = 7 , y = 8
+// console.log("The result is " + timesTotal);
+
+// 4. that execute a the divid function to divid two number
+// x = 10 , y = 2
+// console.log("The result is " + dividTotal);
+
+
+// call the RunThisProgram function
